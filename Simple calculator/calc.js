b5e8aa5b2fa5850f34c348843ff5e1ecc7979f38
chinedugabriel <chinedugabriel29@gmail.com>
2021-11-17 13:51:15 +0100

    change the input type to number

--- Simple calculator/calc.js
+++ Simple calculator/calc.js
@@ -1,21 +1,31 @@
 //this is just a simple function to calculate numbers
 
-function simpleMathProblem(value1, operator, value2){ // *, /, +, -, %
+
+function simpleMathProblem(){ // *, /, +, -, %
+    let value1 = document.getElementById("input1").value;
+    let value2 = document.getElementById("input2").value;
+    let operator = document.getElementById("input3").value;
+    // console.log(value1);
+
+  
     if(operator == "+" || operator == "add"){
-        return value1 + value2;
+        document.getElementById("sol").innerHTML = value1 + value2;
+        result = value1 + value2;
     
     }else if (operator == "-" || operator == "subtract"){
-        return value1 - value2;
+        document.getElementById("sol").innerHTML = value1 - value2;
     
     }else if (operator == "*" || operator == "multiply"){
-        return value1 * value2;
+        document.getElementById("sol").innerHTML = value1 * value2;
     
     }else if (operator == "/" || operator == "divide"){
-        return value1 / value2;
+        document.getElementById("sol").innerHTML = value1 / value2;
     
     }else if (operator == "%" || operator == "modulus"){
-        return value1 % value2;
+        document.getElementById("sol").innerHTML = value1 % value2;
+        
     
     }
+    
 }
 // console.log(simpleMathProblem(12,"divide",4));
